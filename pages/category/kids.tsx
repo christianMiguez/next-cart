369454aaa322caed99e6166224ed26cba7dca393
Christian Miguez <kisquian@gmail.com>
2022-05-02 15:24:24 -0300

    [RESTFUL] get Products & Menu sidebar, search

---
+++ pages/category/kids.tsx
@@ -0,0 +1,29 @@
+import { Typography } from '@mui/material'
+import type { NextPage } from 'next'
+import { ShopLayout } from '../../components/layouts/ShopLayout'
+import { ProductList } from '../../components/products/ProductList'
+import FullScreenLoader from '../../components/ui/FullScreenLoader'
+import { useProducts } from '../../hooks'
+
+const KidPage: NextPage = () => {
+
+  const {products, isLoading} = useProducts('/products?gender=kid')
+
+  return (
+  <ShopLayout title='Serendipia Boutique - Kids' pageDescription='Los mejores articulos para niños'>
+    <Typography variant='h1' component='h1'>Niños</Typography>
+    <Typography variant='h2' sx={{ mb: 1 }}>Productos para niños</Typography>
+
+    {
+      isLoading
+      ? <FullScreenLoader />
+      : <ProductList products={products} />
+    }
+
+    
+    
+  </ShopLayout>
+  )
+}
+
+export default KidPage
---
+++ pages/product/[slug].tsx
@@ -0,0 +1,135 @@
+import { Box, Button, Chip, Grid, Typography } from "@mui/material"
+import { NextPage, GetServerSideProps, GetStaticPaths, GetStaticProps } from "next"
+
+import { useRouter } from "next/router"
+import { ShopLayout } from "../../components/layouts/ShopLayout"
+import ProductSlideshow from "../../components/products/ProductSlideshow"
+import SizeSelector from "../../components/products/SizeSelector"
+import ItemCounter from "../../components/ui/ItemCounter"
+import { dbProducts } from "../../database"
+import { IProduct } from "../../interfaces"
+
+interface Props {
+  product: IProduct
+}
+
+const ProductPage:NextPage<Props> = ({product}) => {
+
+  // const router = useRouter()
+  // const { products: product, isLoading} = useProducts(`/products/${router.query.slug}`)
+
+  return (
+    <ShopLayout title={product.title} pageDescription={product.description}>
+      <Grid container spacing={3}>
+  
+        <Grid item xs={12} sm={7}>
+          <ProductSlideshow images={product.images}/>
+          
+        </Grid>
+
+        <Grid item xs={12} sm={5}>
+          <Box display='flex' flexDirection='column'>
+            <Typography variant='h1' component='h1'>{product.title}</Typography>
+            <Typography variant='subtitle1' component='h2'>${product.price}</Typography>
+
+            <Box sx={{my: 2}}>
+            <Typography variant='subtitle2'>Talle</Typography>
+              <SizeSelector 
+                selectedSize={product.sizes[3]}
+                sizes={product.sizes}
+              />
+              <Typography variant='subtitle2' sx={{mt:3}}>Cantidad</Typography>
+              <ItemCounter />
+              
+            </Box>
+
+            <Button color="secondary" className="circular-btn">
+               Agregar al carrito
+            </Button>
+
+            <Chip label="No hay disponibles" color="error" variant="outlined" sx={{mt: 1}}/>
+
+            <Box sx={{mt: 3}}>
+              <Typography variant='subtitle2'>Descripción</Typography>
+              <Typography variant='body2'>{product.description}</Typography>
+            </Box>
+          </Box>
+          
+        </Grid>
+  
+      </Grid>
+    </ShopLayout>
+  )
+}
+// EXAMPLE 1 OF GET SERVER SIDE PROPS. Works but is not the best option, we can generate it static.
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+// export const getServerSideProps: GetServerSideProps = async ({params}) => {
+//   const { slug } = params as { slug: string }
+//   const product = await dbProducts.getProductBySlug(slug)
+
+//   if (!product) {
+//     return {
+//       redirect: {
+//         destination: '/',
+//         permanent: false,
+//       }
+//     }
+//   }
+
+//   return {
+//     props: {
+//       product
+//     }
+//   }
+// }
+
+
+// EXAMPLE 2: GET SERVER SIDE PROPS WITH STATIC DATA. Better option
+// You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+
+  const productSlugs = await dbProducts.getAllProductsSlugs();
+  
+  return {
+    paths: productSlugs.map(({slug}) =>({
+      params: {
+        slug
+      }
+    })),
+    fallback: "blocking"
+  }
+}
+
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+export const getStaticProps: GetStaticProps = async ({params}) => {
+
+  const {slug = ''} = params as { slug: string }
+  const product = await dbProducts.getProductBySlug(slug);
+
+
+  if (!product) {
+    return {
+      redirect: {
+        destination: '/',
+        // puede ser que mañana tengamos el prod, entonces que Google Bots vuelva a renderizar esto.
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {
+      product
+    }, 
+    // revalidate after 24 hours
+    revalidate: 86400
+  }
+}
+
+export default ProductPage
