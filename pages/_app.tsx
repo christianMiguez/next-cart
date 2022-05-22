ab9a1432712acb51ce3ea507913612a4d10dd60c
Christian Miguez <kisquian@gmail.com>
2022-05-22 17:41:01 -0300

    paypal integration for payment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,40 @@
-import type { AppProps } from 'next/app'
-import {SessionProvider} from 'next-auth/react'
-import { SWRConfig } from 'swr'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { lightTheme } from '../themes'
-import '../styles/globals.css'
-import { AuthProvider, CartProvider, UIProvider } from '../context'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+
+
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { SWRConfig } from 'swr';
+
+import { lightTheme } from '../themes';
+import { AuthProvider, CartProvider, UIProvider } from '../context';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider>
-      <SWRConfig 
-      value={{
-        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-      }}
-    >
-      <AuthProvider>
-        <CartProvider>
-          <UIProvider>
-            <ThemeProvider theme={lightTheme}>
-              <CssBaseline />
-              <Component {...pageProps} />
-            </ThemeProvider>
-          </UIProvider>
-        </CartProvider>
-        </AuthProvider>
-      </SWRConfig>
-    </SessionProvider>
+      <PayPalScriptProvider options={{ 'client-id': process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '' }}>
 
+      
+        <SWRConfig 
+          value={{
+            fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+          }}
+        >
+          <AuthProvider>
+            <CartProvider>
+              <UIProvider>
+                <ThemeProvider theme={ lightTheme}>
+                    <CssBaseline />
+                    <Component {...pageProps} />
+                </ThemeProvider>
+              </UIProvider>
+            </CartProvider>
+          </AuthProvider>
+        </SWRConfig>
+
+      </PayPalScriptProvider>
+    </SessionProvider>
   )
 }
 
